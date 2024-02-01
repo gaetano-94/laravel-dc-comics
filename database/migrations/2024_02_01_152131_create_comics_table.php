380a8e56f69c7e579b6e6f364c60a2b863d7379f
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-01 16:35:16 +0100

    feat: creazione model e database

---
+++ database/migrations/2024_02_01_152131_create_comics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('thumb');
+            $table->string('title', 20);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->decimal('price', 4, 2);
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
